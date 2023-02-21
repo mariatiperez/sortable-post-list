83fa24a85ef8bf6fa635c9a647d2e070b1269b82
Maria Tirado <mariatiperez@gmail.com>
2023-02-21 08:13:58 -0500

    Timeline Item component

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="bg-white drop-shadow-md p-3 flex flex-row content-center justify-between h-[60px]"
+  >
+    <span class="self-center mr-2" data-cy="cell-title">
+      Moved Post {{ itemId }} from Index {{ prevIndex }} to Index
+      {{ currIndex }}
+    </span>
+    <button
+      class="rounded bg-secondary p-2 w-fit self-center"
+      @click="$emit('travel')"
+      data-cy="travel-button"
+    >
+      Time Travel
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  itemId: number;
+  prevIndex: number;
+  currIndex: number;
+}>();
+</script>
---
+++ src/components/__tests__/TimelineItem.cy.ts
@@ -0,0 +1,31 @@
+import TimelineItem from "../TimelineItem.vue";
+
+const props = {
+  itemId: 5,
+  prevIndex: 4,
+  currIndex: 3,
+};
+
+describe("TimelineItem", () => {
+  it("should render title properly", () => {
+    cy.mount(TimelineItem, {
+      props,
+    });
+    cy.get("[data-cy='cell-title']").should(
+      "contain",
+      `Moved Post ${props.itemId} from Index ${props.prevIndex} to Index ${props.currIndex}`
+    );
+    cy.get("[data-cy='travel-button']").should("contain", "Time Travel");
+  });
+
+  it("should emit travel event", () => {
+    const travelEventSpy = cy.spy().as("travelEventSpy");
+
+    cy.mount(TimelineItem, { props: { ...props, onTravel: travelEventSpy } });
+    cy.get("[data-cy='travel-button']")
+      .click()
+      .then(() => {
+        expect(travelEventSpy).to.be.calledOnce;
+      });
+  });
+});
