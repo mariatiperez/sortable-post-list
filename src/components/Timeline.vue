76cfe9387134bec9cc812581acaf9ca88bd889e3
Maria Tirado <mariatiperez@gmail.com>
2023-02-22 20:26:00 -0500

    Improve empty timeline animation and timeline item responsive

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,43 +3,45 @@
     class="h-fit overflow-hidden rounded bg-neutral-100 shadow drop-shadow"
   >
     <h2
-      class="flex h-16 items-center border-neutral-100 bg-white py-3 px-6 text-lg font-medium"
+      class="flex h-16 select-none items-center border-neutral-100 bg-white py-3 px-6 text-lg font-medium"
     >
       List of actions committed
     </h2>
     <div class="flex max-h-[480px] min-h-[70px] items-center p-2 sm:p-6">
-      <div
-        v-if="timeline.length == 0"
-        class="flex h-full w-full flex-col text-center"
-      >
-        No actions to display <br />
-        <span>
-          Click
-          <i class="fa-solid fa-circle-question text-primary" /> to see the
-          instructions
-        </span>
-      </div>
-      <div
-        v-else
-        class="relative max-h-[430px] min-h-[70px] w-full overflow-y-auto overflow-x-clip"
-        id="timeline"
-      >
-        <TransitionGroup name="fade" tag="ul">
-          <TimelineItem
-            v-for="(item, index) in timeline"
-            data-cy="timeline-item"
-            :key="`timeline-item-${item.itemId}-${item.timestamp}`"
-            :item-id="item.itemId"
-            :prev-index="item.prevIndex"
-            :curr-index="item.currIndex"
-            :class="{
-              'rounded-t': index === 0,
-              'rounded-b': index === timeline.length - 1,
-            }"
-            @travel="$emit('travel', index)"
-          />
-        </TransitionGroup>
-      </div>
+      <Transition name="switch">
+        <div
+          v-if="timeline.length == 0"
+          class="flex h-full w-full flex-col text-center"
+        >
+          No actions to display <br />
+          <span>
+            Click
+            <i class="fa-solid fa-circle-question text-primary" /> to see the
+            instructions
+          </span>
+        </div>
+        <div
+          v-else
+          class="relative max-h-[430px] min-h-[70px] w-full overflow-y-auto overflow-x-clip"
+          id="timeline"
+        >
+          <TransitionGroup name="fade" tag="ul">
+            <TimelineItem
+              v-for="(item, index) in timeline"
+              data-cy="timeline-item"
+              :key="`timeline-item-${item.itemId}-${item.timestamp}`"
+              :item-id="item.itemId"
+              :prev-index="item.prevIndex"
+              :curr-index="item.currIndex"
+              :class="{
+                'rounded-t': index === 0,
+                'rounded-b': index === timeline.length - 1,
+              }"
+              @travel="$emit('travel', index)"
+            />
+          </TransitionGroup>
+        </div>
+      </Transition>
     </div>
   </section>
 </template>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,36 +1,39 @@
 <template>
   <li
-    class="flex h-24 w-full select-none flex-row content-center justify-between bg-white p-3 shadow drop-shadow xs:h-16"
+    class="h-30 flex w-full select-none flex-col content-center justify-between bg-white p-3 shadow drop-shadow xs:h-16 xs:flex-row"
   >
-    <span data-cy="timeline-cell-title" class="mr-2 w-3/6 self-center">
+    <span
+      data-cy="timeline-cell-title"
+      class="mr-2 mb-2 w-full self-center xs:mb-0 xs:w-4/6"
+    >
       Moved Post {{ itemId }} from index {{ prevIndex }} to index
       {{ currIndex }}
     </span>
-    <div class="relative">
+    <div class="relative h-9">
       <Transition name="switch">
         <div
           v-if="displayConfirmationDialog"
-          class="flex h-full flex-row content-center"
+          class="mx-auto grid h-full w-4/6 grid-cols-2 justify-items-center xs:w-full"
         >
           <button
             data-cy="confirm-button"
-            class="mr-2 h-9 w-11 min-w-fit self-center rounded bg-primary p-2 text-white"
+            class="h-9 w-11 self-center rounded bg-primary p-2 text-white xs:mr-1"
             @click="handleTravel"
           >
             <i class="fa-solid fa-check" />
           </button>
           <button
             data-cy="cancel-button"
-            class="h-9 w-11 min-w-fit self-center rounded bg-red-400 p-2 text-white"
+            class="h-9 w-11 self-center rounded bg-red-400 p-2 text-white"
             @click="changeConfirmationVisibility(false)"
           >
             <i class="fa-solid fa-xmark" />
           </button>
         </div>
-        <div v-else class="flex h-full flex-row content-center">
+        <div v-else class="grid h-full justify-items-center">
           <button
             data-cy="travel-button"
-            class="h-9 w-24 self-center whitespace-nowrap rounded bg-secondary p-2"
+            class="h-9 w-24 self-center rounded bg-secondary p-2"
             @click="changeConfirmationVisibility(true)"
           >
             Time travel
@@ -61,20 +64,3 @@ const handleTravel = () => {
   emit("travel");
 };
 </script>
-
-<style lang="css" scoped>
-.switch-move,
-.switch-enter-active {
-  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
-  backface-visibility: hidden;
-}
-
-.switch-leave-active {
-  opacity: 0;
-}
-
-.switch-enter-from {
-  opacity: 0;
-  transform: translate(50px, 0);
-}
-</style>
