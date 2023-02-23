ae3b8c5b2cc65ba297969f4f5eda57947da94fe8
Maria Tirado <mariatiperez@gmail.com>
2023-02-22 23:21:00 -0500

    Fix timeline buttons layout

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -4,20 +4,20 @@
   >
     <span
       data-cy="timeline-cell-title"
-      class="mr-2 mb-2 w-full self-center xs:mb-0 xs:w-4/6"
+      class="w-full self-center xs:mb-0 xs:w-4/6"
     >
       Moved Post {{ itemId }} from index {{ prevIndex }} to index
       {{ currIndex }}
     </span>
-    <div class="relative h-9">
+    <div class="relative mx-auto h-9 w-24">
       <Transition name="switch">
         <div
           v-if="displayConfirmationDialog"
-          class="mx-auto grid h-full w-4/6 grid-cols-2 justify-items-center xs:w-full"
+          class="flex h-full w-24 flex-row justify-evenly xs:justify-between"
         >
           <button
             data-cy="confirm-button"
-            class="h-9 w-11 self-center rounded bg-primary p-2 text-white xs:mr-1"
+            class="h-9 w-11 self-center rounded bg-primary p-2 text-white"
             @click="handleTravel"
           >
             <i class="fa-solid fa-check" />
@@ -30,10 +30,10 @@
             <i class="fa-solid fa-xmark" />
           </button>
         </div>
-        <div v-else class="grid h-full justify-items-center">
+        <div v-else class="mx-auto h-full w-24">
           <button
-            data-cy="travel-button"
-            class="h-9 w-24 self-center rounded bg-secondary p-2"
+            data-cy="trave-button"
+            class="h-9 w-full self-center rounded bg-secondary p-2"
             @click="changeConfirmationVisibility(true)"
           >
             Time travel
