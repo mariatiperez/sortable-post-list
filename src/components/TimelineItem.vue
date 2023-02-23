0701af3f436cd0566612a29ea9d0ffeecb307b9c
Maria Tirado <mariatiperez@gmail.com>
2023-02-23 07:38:46 -0500

    Fix timeline item buttons

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -11,36 +11,35 @@
     </p>
 
     <!-- Action buttons -->
-    <div class="relative mx-auto h-9 w-24">
+    <div class="relative mx-auto h-9 w-24 xs:m-0">
       <Transition name="switch">
         <div
           v-if="displayConfirmationDialog"
-          class="flex h-full w-24 flex-row justify-evenly xs:justify-between"
+          class="flex w-24 flex-row justify-evenly xs:justify-between"
         >
           <button
             data-cy="confirm-button"
-            class="h-9 w-11 self-center rounded bg-primary p-2 text-white"
+            class="w-11 self-center rounded bg-primary p-2 text-white"
             @click="handleTravel"
           >
             <i class="fa-solid fa-check" />
           </button>
           <button
             data-cy="cancel-button"
-            class="h-9 w-11 self-center rounded bg-red-400 p-2 text-white"
+            class="w-11 self-center rounded bg-red-400 p-2 text-white"
             @click="changeConfirmationVisibility(false)"
           >
             <i class="fa-solid fa-xmark" />
           </button>
         </div>
-        <div v-else class="mx-auto h-full w-24">
-          <button
-            data-cy="trave-button"
-            class="h-9 w-full self-center rounded bg-secondary p-2"
-            @click="changeConfirmationVisibility(true)"
-          >
-            Time travel
-          </button>
-        </div>
+        <button
+          v-else
+          data-cy="travel-button"
+          class="mx-auto w-24 self-center rounded bg-secondary p-2 xs:mr-0"
+          @click="changeConfirmationVisibility(true)"
+        >
+          Time travel
+        </button>
       </Transition>
     </div>
   </li>
